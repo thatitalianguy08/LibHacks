37b7c3291bd1119c6259973e80c00edf59e70c2f
thatitalianguy08 <anthony.locascio2@outlook.com>
2024-08-02 15:07:14 -0700

    updated

--- src/client/java/com/thatitalianguy/libhacks/LibHacksClient.java
+++ src/client/java/com/thatitalianguy/libhacks/LibHacksClient.java
@@ -1,5 +1,6 @@
 package com.thatitalianguy.libhacks;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
@@ -10,6 +11,7 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.text.Text;
+import net.minecraft.world.GameMode;
 
 public class LibHacksClient implements ClientModInitializer {
 	@Override
@@ -17,12 +19,36 @@ public class LibHacksClient implements ClientModInitializer {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 
 
-		CommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess, environment) -> {
-			dispatcher.register(CommandManager.literal("libFly").executes(context -> {
-				context.getSource().sendFeedback(() -> Text.literal("Called libFly"), true);
+		ClientCommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal("libFly").executes(context -> {
+
 				//MinecraftClient.getInstance().player.getAbilities().setWalkSpeed(1000F);
 				MinecraftClient.getInstance().player.getAbilities().allowFlying = true;
 				MinecraftClient.getInstance().player.getAbilities().flying = true;
+
+				return 1;
+			}));
+			dispatcher.register(ClientCommandManager.literal("libSpeed")
+					.then(ClientCommandManager.argument("speed", IntegerArgumentType.integer())
+							.executes(context -> {
+								int speedVal = IntegerArgumentType.getInteger(context, "speed");
+								float speedValFloat = (float) speedVal;
+								//MinecraftClient.getInstance().player.getAbilities().setWalkSpeed(speedValFloat);
+								MinecraftClient.getInstance().player.setMovementSpeed(speedValFloat);
+
+								return 1;
+							})));
+
+			dispatcher.register(ClientCommandManager.literal("libSetInvincible").executes(context -> {
+				PlayerEntity plr = MinecraftClient.getInstance().player;
+				plr.setInvulnerable(true);
+                if(plr.getAbilities().invulnerable) {
+					//plr.getAbilities().invulnerable = false;
+					plr.setInvulnerable(false);
+				} else if (!plr.getAbilities().invulnerable) {
+					//plr.getAbilities().invulnerable = true;
+					plr.setInvulnerable(true);
+				}
 
 				return 1;
 			}));
